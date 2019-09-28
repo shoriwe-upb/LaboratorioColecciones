4097186a77731d309351a6fa3c2b9aa2837d9fa5
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2019-09-28 12:28:16 -0500

    Update temperature_methods.py
    Added a function to load content froma csv file, eliminated some innecesary 'else'

--- custom_functions/temperature_methods.py
+++ custom_functions/temperature_methods.py
@@ -3,10 +3,21 @@ def _default_help_no_args():
     return 'This option don\'t have arguments'
 
 
+def load_data_from_file(file_path):
+    file = open(file_path)
+    lines = file.readlines()
+    file.close()
+    data = {}
+    for line in lines:
+        department, values = line.strip().split(',')
+        data[department] = tuple(map(lambda value: float(value), values.split(';')))
+    return data
+
+
 # Handler for the functions
 class Analisys(object):
     def __init__(self, data: dict):
-        self.__data = data
+        self._data = data
         # Reference to return month outputs by they name
         self.__ref = (
             'january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october',
@@ -17,42 +28,36 @@ class Analisys(object):
     def department_med(self, department_name):
         if department_name == 'help' or not department_name:
             return "You need to specify the department name"
-        else:
-            return department_name, sum(self.__data[department_name]) / len(self.__data[department_name])
+        return department_name, sum(self._data[department_name]) / len(self._data[department_name])
 
     # Calculate the national temperature med
     def national_med(self, no_arg):
         if no_arg:
             return _default_help_no_args()
-        else:
-            return "National", sum(self.department_med(key)[1] for key in self.__data.keys()) / len(self.__data.keys())
+        return "National", sum(self.department_med(key)[1] for key in self._data.keys()) / len(self._data.keys())
 
     # return the most hot month of every department
     def most_hot(self, no_arg):
         if no_arg:
             return _default_help_no_args()
-        else:
-
-            for key in self.__data.keys():
-                most_hot_month = max(self.__data[key])
-                month = self.__ref[self.__data[key].index(most_hot_month)]
-                yield key, month, most_hot_month
+        for key in self._data.keys():
+            most_hot_month = max(self._data[key])
+            month = self.__ref[self._data[key].index(most_hot_month)]
+            yield key, month, most_hot_month
 
     # Calculate the med betwen the most hot month of three user specified departments
     def med_of_most_hot_month_of_departments(self, departments):
         if departments == "help" or not departments:
             return "Comma separated names of the departments"
-        else:
-            departments = departments.split(",")
-            return departments, sum(max(self.__data[department]) for department in departments) / len(departments)
+        departments = departments.split(",")
+        return departments, sum(max(self._data[department]) for department in departments) / len(departments)
 
     # Calculate the med of all departments and return the higher one
     def most_hot_med(self, departments):
         if departments == "help" or not departments:
             return "Comma separated names of the departments"
-        else:
-            departments = departments.split(",")
-            return max((self.department_med(department) for department in departments), key=lambda value: value[1])
+        departments = departments.split(",")
+        return max((self.department_med(department) for department in departments), key=lambda value: value[1])
 
     # Return the department, month, temperature of all the year
     def most_hot_month_of_year(self, no_arg):
@@ -64,8 +69,8 @@ class Analisys(object):
     def standard_deviation(self, no_arg):
         # Really i dont know f this is the right formula
         if no_arg:
-            _default_help_no_args()
-        for key in self.__data.keys():
-            number_observations = len(self.__data[key])
-            med = sum(self.__data[key]) / number_observations
-            yield key, ((sum((value - med) ** 2 for value in self.__data[key]) / number_observations - 1) ** (1/2))
+            return _default_help_no_args()
+        for key in self._data.keys():
+            number_observations = len(self._data[key])
+            med = sum(self._data[key]) / number_observations
+            yield key, ((sum((value - med) ** 2 for value in self._data[key]) / number_observations - 1) ** (1/2))
